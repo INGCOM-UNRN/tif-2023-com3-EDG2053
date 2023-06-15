9eb6ead858c38dd001e8fc612cf4aa30142ac715
David G <edg20530@gmail.com>
2023-06-15 00:14:52 -0300

    Potable

---
+++ Main.py
@@ -0,0 +1,51 @@
+from func_globales import numero_valido as num_val
+from clasica import Calculadora_Clasica 
+from fraccion import Calculadora_fracciones
+from conversion import Calculadora_Conversion
+
+print("""
+    Para una mejor experiencia le sugerimos 
+        expandir el tamaño de la terminal
+    
+        ---- MENU CALCULADORAS ----
+
+                1 - Clasica
+                2 - Fracciones 
+                3 - Conversiones
+                4 - Salir / off
+
+    """)
+opcion= num_val( input("     Ingrese el numero de calculadora:  ") )
+
+
+while opcion<=0 or opcion>=5:
+    print("\n    Opción no válida, ingrese nuevamente. \n")
+    opcion= num_val( input("     Ingrese el numero de calculadora:  ") )
+
+
+while opcion !=4:
+    if opcion == 1:
+        Calculadora_Clasica()
+
+    elif opcion == 2:
+        Calculadora_fracciones()
+        
+    elif opcion == 3:
+        Calculadora_Conversion()
+        
+    print("""   
+        ---- MENU CALCULADORAS ----
+
+                1 - Clasica
+                2 - Fracciones 
+                3 - Conversiones
+                4 - Salir / off
+    """)
+
+    opcion= num_val( input("     Ingrese el numero de calculadora:  ") )
+    
+
+
+print("""
+        ----- OFF -----
+        """)
